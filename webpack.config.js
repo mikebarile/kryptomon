25e2e11031022486ee5dbcc23d36acbf052b8f8d
Mike Barile <mikebarile13@gmail.com>
2018-01-07 09:44:29 -0800

    Add webpack configuration file.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+module.exports = {
+  context: __dirname,
+  entry: './frontend/cryptomon.jsx',
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  node: {
+  fs: "empty"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  },
+  devtool: 'source-maps'

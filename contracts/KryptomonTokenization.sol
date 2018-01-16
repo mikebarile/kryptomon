1d48d8f0640c8b93a50eafc56298d921d2c5cc60
Mike Barile <mikebarile13@gmail.com>
2018-01-15 19:05:51 -0800

    Clean up some code.

--- contracts/KryptomonTokenization.sol
+++ contracts/KryptomonTokenization.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.11;
 import './GenZeroEggSales.sol';
 
-contract KryptomonOwnership is GenZeroEggSales {
+contract KryptomonTokenization is GenZeroEggSales {
   // function transferEgg(uint32 eggId, address _toAddress) internal {}
 
   // function transferKryptomon(uint32 kryptomonId, address _toAddress) internal {}

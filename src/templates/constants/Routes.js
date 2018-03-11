066cfeecfad99539b880139a894f930413cdcc5e
Alex Doundakov <adoundakov@gmail.com>
2018-03-11 12:31:20 -0700

    add myKryptomon draft, viewEgg, viewGenZeroEgg pages, add MetaMask checker

--- src/templates/constants/Routes.js
+++ src/templates/constants/Routes.js
@@ -2,6 +2,8 @@ export default {
   HOME: '/',
   EGG_STORE: '/egg-store',
   MY_KRYPTOMON: '/ranch',
+  VIEW_GEN_ZERO_EGG: '/egg',
+  VIEW_EGG: '/egg/:egg_id',
   FAQ: '/faq',
   METAMASK: '/get-metamask',
   BESTIARY: '/bestiary',
--- src/templates/misc/EggCard.jsx
+++ src/templates/misc/EggCard.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import {
-  Card,
-  Image,
-  Icon,
-} from 'semantic-ui-react';
+import { Card, Image, Icon } from 'semantic-ui-react';
 
 import EggImg from 'images/logo2.png';
 
-export default function EggStub() {
+export default function EggCard({ link, onClick }) {
+  // stub in case no onClick passed in
+  onClick = onClick || (() => {});
   return (
-    <Card>
+    <Card link={link} onClick={onClick}>
       <Image
         src={EggImg}
         style={{ padding: '34px' }}
@@ -17,13 +15,15 @@ export default function EggStub() {
       />
       <Card.Content>
         <Card.Header>Kryptomon Egg</Card.Header>
-        <Card.Meta><strong>Gen 0</strong></Card.Meta>
+        <Card.Meta>
+          <strong>Gen 0</strong>
+        </Card.Meta>
         <Card.Description>
           A rare Gen 0 egg! What could be inside?
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Icon name='shop' />
+        <Icon name="shop" />
         Bought {Math.floor(Math.random() * 100)} times today!
       </Card.Content>
     </Card>

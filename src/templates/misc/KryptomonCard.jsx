89e341eb19186a435ad9ce13a441b418c5601077
Alex Doundakov <adoundakov@gmail.com>
2018-03-23 15:39:11 -0700

    jazz up KryptomonCard, start breeding calcs in ViewKryptomon

--- src/templates/misc/KryptomonCard.jsx
+++ src/templates/misc/KryptomonCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Label } from 'semantic-ui-react';
+import moment from 'moment';
 
 import { getImageFromKryptomonId } from 'src/util';
 import { rarityById, SpeciesNames } from 'constants/Kryptomon';
@@ -16,16 +17,38 @@ class KryptomonCard extends React.Component {
   };
 
   state = {
-    imgSrc: '',
-    kryptomon: {},
+    imgSrc: '', // Maybe load a placeholder locally?
+    kryptomon: {
+      generation: '--',
+    },
     species: {},
+    evolutionTime: moment('01-01-2090'),
+    breedingTime: moment('01-01-2090'),
   };
 
   async componentDidMount() {
     const imgSrc = await getImageFromKryptomonId(this.props.kryptomonId);
     const kryptomon = await getKryptomon(this.props.kryptomonId).call();
     const species = await getSpeciesDetails(kryptomon.speciesId).call();
-    this.setState({ imgSrc, kryptomon, species });
+    const evolutionTime = moment.unix(
+      Number(kryptomon.birthTimeStamp) + Number(species._timeToEvolve),
+    );
+    const breedingTime = moment.unix(
+      Number(kryptomon.lastBred) + Number(species._breedingCooldown),
+    );
+    this.setState({ imgSrc, kryptomon, species, evolutionTime, breedingTime });
+  }
+
+  isReadyToEvolve() {
+    const { evolutionTime, species } = this.state;
+    return (
+      moment().isSameOrAfter(evolutionTime, 'second') &&
+      species._evolveToId !== '0'
+    );
+  }
+
+  isReadyToBreed() {
+    return moment().isSameOrAfter(this.state.breedingTime, 'second');
   }
 
   render() {
@@ -34,7 +57,7 @@ class KryptomonCard extends React.Component {
       <Card link={this.props.link} onClick={this.props.onClick}>
         <Image
           src={this.state.imgSrc}
-          style={{ width: '100%', height: 'auto' }}
+          style={{ width: '100%', height: 'auto', minHeight: 150 }}
           label={{
             color: rarity.color,
             content: rarity.name,
@@ -50,8 +73,22 @@ class KryptomonCard extends React.Component {
             <strong>Gen {this.state.kryptomon.generation}</strong>
           </Card.Meta>
           <Card.Description>
-            Some basic stats about this Kryptomon! <br />
-            Maybe attack, defense, how old it is?
+            <Label basic color="red" content="TYPE" horizontal />
+            {this.state.species.isExtinct ? (
+              <Label
+                basic
+                icon="exclamation triangle"
+                color="black"
+                content="Extinct"
+                horizontal
+              />
+            ) : null}
+            {this.isReadyToEvolve() ? (
+              <Label basic color="green" content="Ready to Evolve" horizontal />
+            ) : null}
+            {this.isReadyToBreed() ? (
+              <Label basic color="pink" content="Ready to Breed" horizontal />
+            ) : null}
           </Card.Description>
         </Card.Content>
       </Card>

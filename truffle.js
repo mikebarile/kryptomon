d349f8e1f4c3227c3b3d8cde6b826de01fddab65
mikebarile <mikebarile13@gmail.com>
2018-03-24 00:56:12 -0700

    Remove double copy of krypto god from migration.

--- truffle.js
+++ truffle.js
@@ -8,7 +8,7 @@ module.exports = {
    rinkeby: {
       host: "localhost", // Connect to geth on the specified
       port: 8545,
-      from: "0xEc53459d4650964Ee328223a89e176f28E1a5d80", // default address to use for any transaction Truffle makes during migrations
+      from: "0xa9c4a6cb737422f85c2bdac5493dcf7617e86772", // default address to use for any transaction Truffle makes during migrations
       network_id: 4,
       gas: 6712390 // Gas limit used for deploys
     }
